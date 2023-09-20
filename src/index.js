9bea238062bea5f10e2707215efffb414c4d77a6
Jason Phillips <jasonanthonyp@gmail.com>
2023-09-20 19:21:58 -0400

    challenge 2 and 3

--- src/index.js
+++ src/index.js
@@ -7,7 +7,33 @@ fetch("http://localhost:3000/movies")
     movies.forEach(movie => {
         const image = document.createElement("img")
         image.src = movie.image;
+       
+        //console.log(image)
+
+        image.addEventListener("click", () => {
+            document.querySelector("#detail-image").src = movie.image;
+            document.querySelector("#title").textContent = movie.title;
+            document.querySelector("#year-released").textContent = movie.release_year;
+            document.querySelector("#description").textContent = movie.description;
+            document.querySelector("#watched").textContent = movie.watched;
+            document.querySelector("#amount").textContent = movie.blood_amount
+            
+            
+
+        })
+        
         document.querySelector("#movie-list").append(image)
-        console.log(image)
+
     })
+
+    document.querySelector("#detail-image").src = movies[0].image;
+    document.querySelector("#title").textContent = movies[0].title;
+    document.querySelector("#year-released").textContent = movies[0].release_year;
+    document.querySelector("#description").textContent = movies[0].description;
+
+
+
 })
+
+
+// when page loads, first movie should appear
